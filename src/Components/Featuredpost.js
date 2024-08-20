899c588b8fcbe3dd61b3cf620b870a952a707d8f
manishatamang752@gmail.com <manishatamang752@gmail.com>
2024-08-21 00:07:21 +0545

    Recipe app

--- src/Components/Featuredpost.js
+++ src/Components/Featuredpost.js
@@ -2,14 +2,16 @@ import React from "react";
 import food1 from "../Images/pexels-xmtnguyen-699953.jpg";
 import food2 from "../Images/Mexican.jpg";
 import food3 from "../Images/Chinese.jpeg";
+import "./Featuredpost.css";
 
 const Featuredpost = () => {
   return (
-    <div className="f-main">
+    <div className="main">
+      <h2 className="h0"> Featured posts</h2>{" "}
       <div className="food">
-        <img src={food1}></img>
-        <img src={food2}></img>
-        <img src={food3}></img>
+        <img src={food1} alt=""></img>
+        <img src={food2} alt=""></img>
+        <img src={food3} alt=""></img>
       </div>
     </div>
   );
