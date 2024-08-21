9cff9d04543b9712aa3cc3cea99c2935bb498d98
manishatamang752@gmail.com <manishatamang752@gmail.com>
2024-08-21 13:31:05 +0545

    Recipe app

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// import AboutUs from "./Components/AboutUs";
-// import Footer from "./Components/Footer";
-// import Navbar from "./Components/Navbar";
-// import Home from "./Components/Home";
-// import Blog from "./Components/Blog";
-// import AddRecipe from "./Components/AddRecipe";
-import Login from "./Components/Login";
+import AboutUs from "./Components/AboutUs";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Blog from "./Components/Blog";
+import AddRecipe from "./Components/AddRecipe";
+// import Login from "./Components/Login";
 function App() {
   return (
     <div className="App">
       <div>
         <Router>
-          {/* <Navbar /> */}
+          <Navbar />
           <Routes>
-            {/* <Route path="/Home" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
             <Route path="/Blog" element={<Blog />} />
-            <Route path="/AboutUs" element={<AboutUs />} /> */}
-            <Route path="/Login" element={<Login />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+            {/* <Route path="/Login" element={<Login />} /> */}
           </Routes>
         </Router>
       </div>
-      {/* <Home />
+      <Home />
       <AddRecipe />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
