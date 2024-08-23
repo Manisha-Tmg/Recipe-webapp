b87cc02c97c3667efd4154019091b0c035a51eb9
manishatamang752@gmail.com <manishatamang752@gmail.com>
2024-08-23 23:18:30 +0545

    updated code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import AddRecipe from "./pages/AddRecipe";
-// import Signin from "./pages/Signin";
+import Recipe from "./pages/Recipe";
 function App() {
   return (
     <div className="App">
@@ -21,6 +21,7 @@ function App() {
             <Route path="/Blog" element={<Blog />} />
             <Route path="/AboutUs" element={<AboutUs />} />
             <Route path="/AddRecipe" element={<AddRecipe />} />
+            <Route path="/Recipes" element={<Recipe />} />
             {/* <Route path="/Add" element={<Signin />} /> */}
           </Routes>
         </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,12 @@ const Home = () => {
   return (
     <div>
       <Body />
+      {/* <p className="h2">Popular Recipes You Can't Miss</p>
+      <p className="pe">
+        From comfort food classics to exotic flavours, our featured
+      </p>
+      <p className="pe1"> recipes are sure to impress.</p> */}
+
       <Recipe />
       <Services />
     </div>
